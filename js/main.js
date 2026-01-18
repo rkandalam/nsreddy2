12e1efc0407de6d2da604f49ec32c06973c88866
rajkandalam <rajkandalam@gmail.com>
2026-01-18 21:29:30 +0530

    First version

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.nav-link');
+    let currentPage = window.location.pathname.split('/').pop();
+    currentPage = currentPage.replace('.html', '') || 'home';
 
+    links.forEach(link => {
+        if (link.dataset.page === currentPage) {
+            link.classList.add('active');
+        }
+    });
+});
